b5350780dcf3d7121c154d62ca80f0579ad5a730
Hussain Jamal <hjamal9865@gmail.com>
2026-06-25 11:45:31 +0500

    first commit

---
+++ event-bus/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+app.use(express.json());
+
+app.post("/events", (req, res) => {
+    const event = req.body;
+    // post to posts service
+    axios.post("http://localhost:4000/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // post to comments service
+    axios.post("http://localhost:4001/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // post to query service
+    axios.post("http://localhost:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    res.send({ status: "OK" });
+});
+
+app.listen(4005, () => {
+    console.log("Listening on port 4005");
+});
